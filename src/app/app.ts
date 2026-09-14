922a9309952929a02cbcedb030e2c10ab063be20
sy <erica860885@gmail.com>
2026-09-14 23:56:24 +0800

    feat:建立Hero Component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { Header } from './header/header';
+import { Hero } from './hero/hero';
 
 @Component({
   selector: 'app-root',
-  imports: [Header],
+  imports: [Header,Hero],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
