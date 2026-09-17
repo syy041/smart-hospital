effc3c5be955bda91001faf5d511025173baa2fd
sy <erica860885@gmail.com>
2026-09-18 01:23:03 +0800

    feat: 引入QuickService元件至AppComponent

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { Header } from './header/header';
 import { Hero } from './hero/hero';
+import { QuickService } from './quick-service/quick-service';
 
 @Component({
   selector: 'app-root',
-  imports: [Header,Hero],
+  imports: [Header,Hero, QuickService],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
